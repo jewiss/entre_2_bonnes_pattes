3885bd925ca6f3189ded94c0a9ebaac77ac2d9c3
patrice jaszczynski <ppastg92@free.fr>
2020-04-14 15:58:41 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :animals do
     resources :bookings, only: [:new, :create]
+    resources :bleeds
+    resources :species
   end
-
-  resources :bleeds
-
-  resources :species
 
   resources :users, except: [:index, :destroy] do
     resources :bookings, only: [:create, :edit, :update]

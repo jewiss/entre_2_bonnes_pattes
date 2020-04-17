b362bc98e3dfa37255c42d2b735beef5bd8664d2
patrice jaszczynski <ppastg92@free.fr>
2020-04-17 20:26:58 +0200

    Ajout d'un formulaire multi étapes + Changement logo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,14 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("turbolinks").start()
 
 import "bootstrap";
+
+
+document.addEventListener('turbolinks:load', () => {
+
+require("./multistep_form").reload()
+
+})
+

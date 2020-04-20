23dabcbf099313b227201955b0ce1a19f76d59f3
patrice jaszczynski <ppastg92@free.fr>
2020-04-20 16:46:42 +0200

    Ajout autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,13 @@
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initAutocomplete();
+});

b6fb7a0a297efa6b3fc008d08857e5c1283cb16e
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-04-23 14:21:16 +0200

    creating 10 users faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 users'
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: "123456",
+    admin: false,
+    last_name: Faker::name.last_name,
+    first_name: Faker::name.first_name,
+    age: rand(20..50),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence
+    )
+  "users created !"
+end

f168d81f2116a0256e151d5e26409db4eae876a2
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-04-29 18:59:50 +0200

    re-push commitment

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :age, :address, presence: true
   after_validation :geocode, if: :will_save_change_to_address?
+
 end

d3c2b2f2b1cbb3736f4f5172ad54eff499369cfb
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-05-01 17:20:13 +0200

    address user modifiee

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ puts"Deleting users..."
 User.delete_all
 
 puts 'Creating 10 new users...'
+number_address = 1
 10.times do
   user = User.new(
     email: Faker::Internet.email,
@@ -12,13 +13,15 @@ puts 'Creating 10 new users...'
     last_name: Faker::Name.last_name,
     first_name: Faker::Name.first_name,
     age: rand(Date.civil(1975, 1, 1)..Date.civil(2019, 12, 31)),
-    address: Faker::Address.full_address,
+    address: "#{number_address} avenue Champs Elysées, 75008 PARIS FRANCE" ,
     description: Faker::Lorem.sentence
     )
  user.save!
+ number_address += 10
 end
 puts "users created !"
 
+puts"Deleting species..."
 
 Specie.delete_all
 puts 'Creating Species'
@@ -28,6 +31,8 @@ species.each do |specie|
   x.save!
 end
 puts "species created !"
+
+puts"Deleting bleeds..."
 
 Bleed.delete_all
 puts 'Creating Breeds'

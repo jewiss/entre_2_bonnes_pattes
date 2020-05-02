d2eb283381fc17bebed9ca6ecabefd5d957e17a8
patrice jaszczynski <ppastg92@free.fr>
2020-05-02 18:49:54 +0200

    Creation dashboard profil user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @users = policy_scope(User)
   end
 
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+    @old = old
+  end
+
+  private
+
+  def old
+    now = Time.now.utc.to_date
+    birthday = @user.age
+    now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+
 end
